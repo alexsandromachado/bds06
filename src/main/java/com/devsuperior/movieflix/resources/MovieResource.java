07c6c2458490d8416d5c38978f07b2493aabdb3c
Alexsandro Machado <lekomachado@hotmail.com>
2022-06-02 15:40:05 -0300

    initial configurations and classes

---
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -0,0 +1,12 @@
+package com.devsuperior.movieflix.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieResource {
+	
+
+}
